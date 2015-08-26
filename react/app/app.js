b41e514469875b2625fe94544f25eb1034af8048
Martin Brochhaus <mbrochh@gmail.com>
2015-08-26 13:02:23 +0200

    Added Router, one wrapper component and two view components

--- react/app/app.js
+++ react/app/app.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { Router, Route } from "react-router";
+import { history } from "react-router/lib/HashHistory";
 
+import App from "./components/App.jsx";
 import MyComponent from "./components/MyComponent.jsx";
+import OtherComponent from "./components/OtherComponent.jsx";
+
+const routes = (
+  <Route component={App}>
+    <Route path="/" component={MyComponent} />
+    <Route path="/other" component={OtherComponent} />
+  </Route>
+);
+
+const element = (
+  <Router history={history} children={routes} />
+);
 
 let container = document.createElement("div");
 document.body.appendChild(container);
-React.render(
-  React.createElement(MyComponent), container);
+React.render(element, container);
---
+++ react/app/components/OtherComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class OtherComponent extends React.Component {
+  render() {
+    return (
+      <div>Other</div>
+    );
+  }
+}
